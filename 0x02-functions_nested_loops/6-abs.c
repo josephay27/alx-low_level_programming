4b9a51d8dcd48bb77c0feb59cbffd7d80215c40b
Joseph <you@example.com>
2023-06-20 15:02:32 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * computes absolute value of an integer
+ * Return: absolute value of the input number.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
