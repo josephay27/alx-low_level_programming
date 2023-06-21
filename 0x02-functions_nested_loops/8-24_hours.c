69121e86ce261c6dc7815073766ec698de13b83d
Joseph <you@example.com>
2023-06-20 17:39:12 -0700

    24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * prints every minute of the day
- * Return: Always 0
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * starting from 00:00 to 23:59
+ *
  */
 void jack_bauer(void)
 {
